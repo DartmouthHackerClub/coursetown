8be4a13d189a7fbde94979da31f8ee1d7be922b1
Kyle Konrad <kyle.t.konrad@gmail.com>
2011-09-30 17:45:40 -0400

    first rails commit

---
+++ coursetown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursetown::Application.config.secret_token = 'e385198f28ed0de57beaa63e381957e167d6396f7ec5c2a01498164f5106bf6f7da640b00762d72f21f63a7d6674eb7808184b762608ce18248026d9dcc30943'
