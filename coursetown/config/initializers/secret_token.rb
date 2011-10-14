b2f42f486c0346e18d8acb803fad13b52fd039f2
Marco A. Peraza <marcoaperaza@gmail.com>
2011-10-14 17:27:16 -0400

    Add rails 3.1 project

---
+++ coursetown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursetown::Application.config.secret_token = '14ab571e5fcec0f886104346f372bc20c29f2456e7d0c9bb7dcfa5c26bf6998892492aec118e1484f3ea381d8978c949a3586b50a0d002bbead820f085bab0b8'
