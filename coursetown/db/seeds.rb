84aef6cb5ee16f9d5cb354afd79bf49032ab90b4
Kyle Konrad <kyle.t.konrad@gmail.com>
2011-10-21 16:50:38 -0400

    fixed weirdness in offerings table. seed data

--- coursetown/db/seeds.rb
+++ coursetown/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+courses = Course.create([{department: 'Computer Science', number: 5, title: 'Introduction'}, {department: 'Mathematics', number: 22, title: 'Linear Algebra'}])
+
+Offering.create([{course_id: courses.first.id, year: 2011, term: 'F', professor: 'Scott Drysdale', time: "10A"}, {course_id: courses.first.id, year: 2011, term: 'W', professor: 'Chris Bailey-Kellogg', time: "2"}])
