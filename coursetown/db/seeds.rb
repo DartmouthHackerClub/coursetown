f2243884111de1bf307c5bcdaffacd4db4479719
hac <hactheplanet@gmail.com>
2011-10-21 18:26:54 -0400

    Seeded schedules.

--- coursetown/db/seeds.rb
+++ coursetown/db/seeds.rb
@@ -6,10 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-courses = Course.create([{department: 'Computer Science', number: 5, title: 'Introduction'}, {department: 'Mathematics', number: 22, title: 'Linear Algebra'}, {department: 'Wizardry', number: 17, title: 'Care of Magical Creatures'}, {department: 'Wizardry', number: 43, title: 'Defence Against the Dark Arts'}])
+courses = Course.create([
+  {department: 'Computer Science', number: 5, title: 'Introduction'},
+  {department: 'Mathematics', number: 22, title: 'Linear Algebra'},
+  {department: 'Wizardry', number: 17, title: 'Care of Magical Creatures'},
+  {department: 'Wizardry', number: 43, title: 'Defence Against the Dark Arts'}])
 
-Offering.create([{course_id: courses.first.id, year: 2011, term: 'F', professor: 'Scott Drysdale', time: "10A"}, {course_id: courses.first.id, year: 2011, term: 'W', professor: 'Chris Bailey-Kellogg', time: "2"}])
+users = User.create([{name: "Octavius Ott"}, {name: "Cindy Ott"}, {name: "John Ledyard"}])
 
-users = Users.create([{name: "Octavius Ott"}, {name: "Cindy Ott"}, {name: "John Ledyard"}])
+offerings = Offering.create([
+  {course_id: courses.first.id, year: 2011, term: 'F', professor: 'Scott Drysdale', time: "10A"},
+  {course_id: courses.first.id, year: 2011, term: 'W', professor: 'Chris Bailey-Kellogg', time: "2"},
+  {course_id: courses[2].id, year: 2011, term: 'F', professor: 'Hagrid', time: "8"},
+  {course_id: courses[2].id, year: 2011, term: 'W', professor: 'Hagrid', time: "8"}])
 
-Wishlists.create([{user_id: users[0], course_id: courses[1]}, {user_id: users[0], course_id: courses[3]}, {user_id: users[2], course_id: courses[2]}, {user_id: users[2], course_id: courses[1]}])
+Wishlist.create([
+  {user_id: users[0].id, course_id: courses[1].id}, {user_id: users[0].id, course_id: courses[3].id},
+  {user_id: users[2].id, course_id: courses[1].id}])
+
+Schedule.create([
+  {user_id: users[0].id, offering_id: offerings[0].id}, {user_id: users[0].id, offering_id: offerings[2].id},
+  {user_id: users[2].id, offering_id: offerings[1].id}, {user_id: users[2].id, offering_id: offerings[3].id}])
