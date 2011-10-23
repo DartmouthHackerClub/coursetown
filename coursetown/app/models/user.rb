0f66bbcc846c4108093625f09edaf91a54a27d6a
Cole Ott <cole.ott@gmail.com>
2011-10-23 16:41:28 -0400

    user model: added wishlist_offerings etc
    smart properties that do common sql queries in nice ways:
    wishlist_offerings, wishlist_courses, schedule_offerings

--- coursetown/app/models/user.rb
+++ coursetown/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :courses, :through => :wishlist
-  has_many :offerings, :through => :schedule
+  has_many :wishlists
+  has_many :wishlist_courses, :through => :wishlists, :source => :course
+  has_many :wishlist_offerings, :through => :wishlist_courses, :source => :offerings
+
+  has_many :schedules
+  has_many :schedule_offerings, :through => :schedules, :source => :offering
 end
