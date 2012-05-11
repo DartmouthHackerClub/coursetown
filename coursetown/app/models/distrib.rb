8c09ffa02813558ea252b4ed62cf9e5996fb1502
Cole Ott <cole.ott@gmail.com>
2012-05-11 01:50:14 -0400

    added prof & distrib helper methods

--- coursetown/app/models/distrib.rb
+++ coursetown/app/models/distrib.rb
@@ -1,3 +1,36 @@
 class Distrib < ActiveRecord::Base
   belongs_to :offering
+  validates :distrib_abbr, :inclusion => {:in => @abbr_set,
+    :message => "Invalid distrib abbreviation: '%{value}'"}
+
+  @full_names = {
+    'INT' => 'International or Comparative Study',
+    'ART' => 'Arts',
+    'LIT' => 'Literature',
+    'SCI' => 'Natural Science',
+    'SLA' => 'Natural Science (Lab)',
+    'QDS' => 'Quantitative & Deductive Science',
+    'SOC' => 'Social Analysis',
+    'TAS' => 'Technology or Applied Science',
+    'TLA' => 'Technology or Applied Science (Lab)',
+    'TMV' => 'Traditions of Thought, Meaning & Value'
+  }
+  @abbr_set = Set.new(@full_names.each_key.to_a)
+
+  def self.full_name(distrib_abbr)
+    @full_names[distrib_abbr]
+  end
+
+  def full_name
+    self.class.full_name(self.distrib_abbr)
+  end
+
+  def self.all_abbrs
+    @full_names.each_key
+  end
+
+  def self.is_abbr?(str)
+    @abbr_set.include? str
+  end
+
 end
