b118c26e007bfef4a5473328e126e8855a1974f5
Cole Ott <cole.ott@gmail.com>
2012-05-18 01:21:16 -0400

    asset pipeline workaround! css -> public, tweaked configs

--- coursetown/config/environments/production.rb
+++ coursetown/config/environments/production.rb
@@ -6,16 +6,16 @@ Coursetown::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false # changed
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true # changed
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # changed
 
   # Generate digests for assets URLs
   config.assets.digest = true
