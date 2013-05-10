e6cca4c4b6074bb7990830559898224127ea72cb
Zach Denton <zacharydenton@gmail.com>
2013-05-10 17:19:28 -0400

    Index a few more fields.

--- app/indices/offering_index.rb
+++ app/indices/offering_index.rb
@@ -1,6 +1,9 @@
 ThinkingSphinx::Index.define :offering, :with => :active_record do
+  indexes courses.short_title
   indexes courses.long_title
   indexes courses.desc
+  indexes courses.department
+  indexes courses.number
   indexes professors.name
 
   has median_grade, created_at, updated_at
